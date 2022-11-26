0b6b59e6b793788f3ea0115ce11cdad94c8e0f05
Cuneyt Oner <cuneytoner@gmail.com>
2022-11-26 17:46:44 +0300

    Add files via upload

--- mssql.sh
+++ mssql.sh
@@ -7,7 +7,7 @@ fi
 
 if ! docker ps -a --format '{{.Names}}' | grep -w devmssql &> /dev/null; then
    echo "Docker is going to run" 
-   docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Password1!' -p 1433:1433 -v /docker/mssql:/var/opt/mssql/data -v /docker/mssql/log:/var/opt/mssql/log -v /docker/mssql/secrets:/var/opt/mssql/secrets -d mcr.microsoft.com/mssql/server:2019-latest
+   docker run --name devmssql -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Password1!' -p 1433:1433 -v /docker/mssql:/var/opt/mssql/data -v /docker/mssql/log:/var/opt/mssql/log -v /docker/mssql/secrets:/var/opt/mssql/secrets -d mcr.microsoft.com/mssql/server:2019-latest
 else
    if docker ps -a --format '{{.Names}} {{.Status}}' | grep -w devmssql | grep -w Up &> /dev/null; then
       echo "Docker running, is going to stop"
